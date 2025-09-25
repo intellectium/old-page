ffd459a870a382f61ebf2ecbd85aaa07fbc78638
mike <jfkz@users.noreply.github.com>
2025-09-25 07:11:57 +0000

    feat: initial commit

---
+++ mental/security.js
@@ -0,0 +1,7 @@
+var pass = prompt("Password (ask your Intellectium teacher):", "");
+if (pass == null)
+  window.location = "http://intellectium.ru/tr/badpassword";
+else if (pass.toLowerCase() == "mental99")
+  window.location = "http://intellectium.ru/mental/intellectiummentalonlinetestforkids1009171147v3";
+else
+  window.location = "http://intellectium.ru";
